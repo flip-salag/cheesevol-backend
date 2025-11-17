8ef1482bfd1bd03f595de789de8128a7e49b072c
Jaemin <ghdckdwn8456@gmail.com>
2025-11-18 03:23:07 +0900

    [feat] Novel mapper 수정 - #17

--- novelservice/src/main/java/com/iucyh/novelservice/novel/web/dto/mapper/NovelRequestMapper.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/web/dto/mapper/NovelRequestMapper.java
@@ -1,18 +1,18 @@
 package com.iucyh.novelservice.novel.web.dto.mapper;
 
 import com.iucyh.novelservice.novel.enumtype.NovelCategory;
-import com.iucyh.novelservice.novel.domain.Novel;
+import com.iucyh.novelservice.novel.service.command.CreateNovelCommand;
 import com.iucyh.novelservice.novel.web.dto.request.CreateNovelRequest;
-import com.iucyh.novelservice.user.domain.User;
 
 public class NovelRequestMapper {
 
     private NovelRequestMapper() {}
 
-    public static Novel toNovel(CreateNovelRequest request) {
-        NovelCategory category = NovelCategory.of(request.category());
-        // TODO: 실제 User 엔티티 받도록 수정
-        User user = User.of("abc@example.com", "abc123", "lucy", "test");
-        return Novel.of(request.title(), request.description(), category, user);
+    public static CreateNovelCommand toCreateNovelCommand(CreateNovelRequest request) {
+        return new CreateNovelCommand(
+                request.title(),
+                request.description(),
+                NovelCategory.of(request.category())
+        );
     }
 }

5bb9314f51f7fe119d57bc926a2029a3420873d7
Jaemin <ghdckdwn8456@gmail.com>
2025-11-28 12:12:48 +0900

    [feat] 업데이트순 정렬 구현 - #19

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelLastUpdatePagingStrategy.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelLastUpdatePagingStrategy.java
@@ -2,15 +2,12 @@ package com.iucyh.novelservice.novel.repository.query.paging;
 
 import com.iucyh.novelservice.novel.domain.Novel;
 import com.iucyh.novelservice.novel.repository.query.dto.NovelQueryDto;
-import com.iucyh.novelservice.novel.repository.query.dto.QNovelSimpleQueryDto;
 import com.iucyh.novelservice.novel.repository.query.paging.cursor.NovelCursor;
 import com.iucyh.novelservice.novel.repository.query.paging.cursor.NovelLastUpdateCursor;
 import com.iucyh.novelservice.novel.enumtype.NovelSortType;
 import com.iucyh.novelservice.novel.repository.query.paging.template.AbstractNovelPagingStrategy;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Component;
 
 import static com.iucyh.novelservice.novel.domain.QNovel.novel;
@@ -19,16 +16,10 @@ import static com.iucyh.novelservice.novel.domain.QNovel.novel;
 public class NovelLastUpdatePagingStrategy extends AbstractNovelPagingStrategy {
 
     @Override
-    protected JPAQuery<? extends NovelQueryDto> createBaseQuery(JPAQueryFactory queryFactory) {
-        return queryFactory
-                .select(new QNovelSimpleQueryDto(novel))
-                .from(novel);
-    }
-
-    @Override
     protected OrderSpecifier<?>[] applyOrder() {
         return new OrderSpecifier[] {
                 novel.lastEpisodeAt.desc(),
+                novel.createdAt.desc(),
                 novel.id.desc()
         };
     }
@@ -36,17 +27,28 @@ public class NovelLastUpdatePagingStrategy extends AbstractNovelPagingStrategy {
     @Override
     protected BooleanExpression applyCursor(NovelCursor cursor) {
         NovelLastUpdateCursor lastUpdateCursor = (NovelLastUpdateCursor) cursor;
-        return novel.lastEpisodeAt.lt(lastUpdateCursor.lastUpdateDate())
+        return novel.lastEpisodeAt.lt(lastUpdateCursor.lastEpisodeAt())
                 .or(
-                        novel.lastEpisodeAt.eq(lastUpdateCursor.lastUpdateDate())
-                                .and(novel.id.lt(lastUpdateCursor.lastNovelId()))
+                        novel.lastEpisodeAt.eq(lastUpdateCursor.lastEpisodeAt())
+                                .and(
+                                        novel.createdAt.lt(lastUpdateCursor.lastCreatedAt())
+                                )
+                )
+                .or(
+                        novel.lastEpisodeAt.eq(lastUpdateCursor.lastEpisodeAt())
+                                .and(
+                                        novel.createdAt.eq(lastUpdateCursor.lastCreatedAt())
+                                )
+                                .and(
+                                        novel.id.lt(lastUpdateCursor.lastNovelId())
+                                )
                 );
     }
 
     @Override
     public NovelCursor createCursor(NovelQueryDto lastResult) {
         Novel lastNovel = lastResult.getNovel();
-        return new NovelLastUpdateCursor(lastNovel.getId(), lastNovel.getLastEpisodeAt());
+        return new NovelLastUpdateCursor(lastNovel.getId(), lastNovel.getLastEpisodeAt(), lastNovel.getCreatedAt());
     }
 
     @Override
--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelLastUpdateCursor.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelLastUpdateCursor.java
@@ -5,5 +5,6 @@ import java.time.LocalDateTime;
 public record NovelLastUpdateCursor(
 
         long lastNovelId,
-        LocalDateTime lastUpdateDate
+        LocalDateTime lastEpisodeAt,
+        LocalDateTime lastCreatedAt
 ) implements NovelCursor {}

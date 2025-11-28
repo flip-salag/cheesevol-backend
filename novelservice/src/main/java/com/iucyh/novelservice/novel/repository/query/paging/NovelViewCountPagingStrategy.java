77e2ee9baa59f1f3281fbaa6010c3f5380618831
Jaemin <ghdckdwn8456@gmail.com>
2025-11-28 12:27:30 +0900

    [feat] 조회순 정렬 구현 - #19

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelViewCountPagingStrategy.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelViewCountPagingStrategy.java
@@ -2,15 +2,12 @@ package com.iucyh.novelservice.novel.repository.query.paging;
 
 import com.iucyh.novelservice.novel.domain.Novel;
 import com.iucyh.novelservice.novel.repository.query.dto.NovelQueryDto;
-import com.iucyh.novelservice.novel.repository.query.dto.QNovelSimpleQueryDto;
 import com.iucyh.novelservice.novel.repository.query.paging.cursor.NovelCursor;
 import com.iucyh.novelservice.novel.repository.query.paging.cursor.NovelViewCountCursor;
 import com.iucyh.novelservice.novel.enumtype.NovelSortType;
 import com.iucyh.novelservice.novel.repository.query.paging.template.AbstractNovelPagingStrategy;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Component;
 
 import static com.iucyh.novelservice.novel.domain.QNovel.novel;
@@ -19,34 +16,39 @@ import static com.iucyh.novelservice.novel.domain.QNovel.novel;
 public class NovelViewCountPagingStrategy extends AbstractNovelPagingStrategy {
 
     @Override
-    protected JPAQuery<? extends NovelQueryDto> createBaseQuery(JPAQueryFactory queryFactory) {
-        return queryFactory
-                .select(new QNovelSimpleQueryDto(novel))
-                .from(novel);
-    }
-
-    @Override
     protected OrderSpecifier<?>[] applyOrder() {
         return new OrderSpecifier[] {
                 novel.totalViewCount.desc(),
+                novel.lastEpisodeAt.desc(),
                 novel.id.desc()
         };
     }
 
     @Override
     protected BooleanExpression applyCursor(NovelCursor cursor) {
-        NovelViewCountCursor novelViewCountCursor = (NovelViewCountCursor) cursor;
-        return novel.totalViewCount.lt(novelViewCountCursor.lastTotalViewCount())
+        NovelViewCountCursor viewCountCursor = (NovelViewCountCursor) cursor;
+        return novel.totalViewCount.lt(viewCountCursor.lastTotalViewCount())
                 .or(
-                        novel.totalViewCount.eq(novelViewCountCursor.lastTotalViewCount())
-                                .and(novel.id.lt(novelViewCountCursor.lastNovelId()))
+                        novel.totalViewCount.eq(viewCountCursor.lastTotalViewCount())
+                                .and(
+                                        novel.lastEpisodeAt.lt(viewCountCursor.lastEpisodeAt())
+                                )
+                )
+                .or(
+                        novel.totalViewCount.eq(viewCountCursor.lastTotalViewCount())
+                                .and(
+                                        novel.lastEpisodeAt.eq(viewCountCursor.lastEpisodeAt())
+                                )
+                                .and(
+                                        novel.id.lt(viewCountCursor.lastNovelId())
+                                )
                 );
     }
 
     @Override
     public NovelCursor createCursor(NovelQueryDto lastResult) {
         Novel lastNovel = lastResult.getNovel();
-        return new NovelViewCountCursor(lastNovel.getId(), lastNovel.getTotalViewCount());
+        return new NovelViewCountCursor(lastNovel.getId(), lastNovel.getTotalViewCount(), lastNovel.getLastEpisodeAt());
     }
 
     @Override

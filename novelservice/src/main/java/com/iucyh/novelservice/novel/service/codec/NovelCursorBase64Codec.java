771b7e14e9564ef727883c48c8e1959dca821cbe
Jaemin <ghdckdwn8456@gmail.com>
2025-12-06 10:26:29 +0900

    [feat] NovelCursorBase64Codec->NovelCursorCodec 인터페이스 구현하도록 수정 - #29

--- novelservice/src/main/java/com/iucyh/novelservice/novel/service/codec/NovelCursorBase64Codec.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/service/codec/NovelCursorBase64Codec.java
@@ -8,15 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class NovelCursorBase64Codec {
+public class NovelCursorBase64Codec implements NovelCursorCodec {
 
     private final Base64Util base64Util;
 
+    @Override
     public String encode(NovelCursor cursor) {
         return base64Util.encode(cursor);
     }
 
-    public <T extends NovelCursor> NovelCursor decode(String cursor, Class<T> type) {
+    @Override
+    public <T extends NovelCursor> NovelCursor decode(String cursor, Class<T> type) throws InvalidNovelCursor {
         if (cursor == null || cursor.isBlank()) {
             return null;
         }

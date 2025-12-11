7d8107cade587736b7d2ef863c9b61bd267e4b31
Jaemin <ghdckdwn8456@gmail.com>
2025-12-11 15:31:39 +0900

    [chore] NovelQueryRepository->각 메서드 반환값 NovelSimpleQueryProjection 으로 변경 - #44

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepository.java
@@ -1,9 +1,9 @@
 package com.iucyh.novelservice.novel.repository.query;
 
 import com.iucyh.novelservice.novel.enumtype.NovelCategory;
-import com.iucyh.novelservice.novel.repository.query.projection.NovelQueryProjection;
 import com.iucyh.novelservice.novel.repository.query.condition.NovelPagingCondition;
 import com.iucyh.novelservice.novel.repository.query.paging.NovelPagingStrategy;
+import com.iucyh.novelservice.novel.repository.query.projection.NovelSimpleQueryProjection;
 
 import java.util.List;
 
@@ -12,11 +12,11 @@ public interface NovelQueryRepository {
     /**
      * @param category 필터링 할 카테고리, 모든 카테고리 조회 시 null 전달
      */
-    List<? extends NovelQueryProjection> findNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category);
+    List<? extends NovelSimpleQueryProjection> findNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category);
 
     /**
      * 이번달 신작 소설 조회 메서드
      * @param category 필터링 할 카테고리, 모든 카테고리 조회 시 null 전달
      */
-    List<? extends NovelQueryProjection> findNewNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category);
+    List<? extends NovelSimpleQueryProjection> findNewNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category);
 }
--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepositoryImpl.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepositoryImpl.java
@@ -1,9 +1,9 @@
 package com.iucyh.novelservice.novel.repository.query;
 
 import com.iucyh.novelservice.novel.enumtype.NovelCategory;
-import com.iucyh.novelservice.novel.repository.query.projection.NovelQueryProjection;
 import com.iucyh.novelservice.novel.repository.query.condition.NovelPagingCondition;
 import com.iucyh.novelservice.novel.repository.query.paging.NovelPagingStrategy;
+import com.iucyh.novelservice.novel.repository.query.projection.NovelSimpleQueryProjection;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<? extends NovelQueryProjection> findNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category) {
+    public List<? extends NovelSimpleQueryProjection> findNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category) {
         return strategy
                 .createQuery(queryFactory, condition.cursor())
                 .where(
@@ -33,7 +33,7 @@ public class NovelQueryRepositoryImpl implements NovelQueryRepository {
     }
 
     @Override
-    public List<? extends NovelQueryProjection> findNewNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category) {
+    public List<? extends NovelSimpleQueryProjection> findNewNovels(NovelPagingCondition condition, NovelPagingStrategy strategy, NovelCategory category) {
         LocalDateTime thisMonth = getThisMonth();
         return strategy
                 .createQuery(queryFactory, condition.cursor())

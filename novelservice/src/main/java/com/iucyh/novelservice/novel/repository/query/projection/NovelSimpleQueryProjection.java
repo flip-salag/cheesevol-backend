68b9b5351c4be328e0376551eee6c2ab861c6fa3
Jaemin <ghdckdwn8456@gmail.com>
2025-12-11 15:19:08 +0900

    [refactor] NovelSimepleQueryProjection 평탄화 - #44

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/projection/NovelSimpleQueryProjection.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/projection/NovelSimpleQueryProjection.java
@@ -1,16 +1,44 @@
 package com.iucyh.novelservice.novel.repository.query.projection;
 
-import com.iucyh.novelservice.novel.domain.Novel;
+import com.iucyh.novelservice.novel.enumtype.NovelCategory;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 @Getter
 public class NovelSimpleQueryProjection {
 
-    private final Novel novel;
+    private final String publicId;
+    private final Long userId;
+    private final String userNickname;
+    private final String title;
+    private final String description;
+    private final NovelCategory category;
+    private final Integer likeCount;
+    private final Integer totalViewCount;
+    private final Boolean isCompleted;
+    private final Long updatedAt;
+    private final Long createdAt;
 
     @QueryProjection
-    public NovelSimpleQueryProjection(Novel novel) {
-        this.novel = novel;
+    public NovelSimpleQueryProjection(
+            String publicId,
+            Long userId, String userNickname,
+            String title, String description,
+            NovelCategory category,
+            Integer likeCount, Integer totalViewCount,
+            Boolean isCompleted,
+            Long updatedAt, Long createdAt
+    ) {
+        this.publicId = publicId;
+        this.userId = userId;
+        this.userNickname = userNickname;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.likeCount = likeCount;
+        this.totalViewCount = totalViewCount;
+        this.isCompleted = isCompleted;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
     }
 }

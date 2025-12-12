050d9d052dfaefc4f14205c45c836cbb21c251a9
Jaemin <ghdckdwn8456@gmail.com>
2025-12-12 11:40:18 +0900

    Revert "[delete] NovelQueryProjection 인터페이스 삭제 - #44"
    This reverts commit a951abfaf23cf904a0679aba690685065e1e1640.

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/projection/NovelSimpleQueryProjection.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/projection/NovelSimpleQueryProjection.java
@@ -1,5 +1,6 @@
 package com.iucyh.novelservice.novel.repository.query.projection;
 
+import com.iucyh.novelservice.novel.domain.Novel;
 import com.iucyh.novelservice.novel.enumtype.NovelCategory;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
@@ -7,40 +8,12 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-public class NovelSimpleQueryProjection {
+public class NovelSimpleQueryProjection implements NovelQueryProjection {
 
-    private final String publicId;
-    private final String userPublicId;
-    private final String userNickname;
-    private final String title;
-    private final String description;
-    private final NovelCategory category;
-    private final Integer likeCount;
-    private final Integer totalViewCount;
-    private final Boolean isCompleted;
-    private final LocalDateTime updatedAt;
-    private final LocalDateTime createdAt;
+    private final Novel novel;
 
     @QueryProjection
-    public NovelSimpleQueryProjection(
-            String publicId,
-            String userPublicId, String userNickname,
-            String title, String description,
-            NovelCategory category,
-            Integer likeCount, Integer totalViewCount,
-            Boolean isCompleted,
-            LocalDateTime updatedAt, LocalDateTime createdAt
-    ) {
-        this.publicId = publicId;
-        this.userPublicId = userPublicId;
-        this.userNickname = userNickname;
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.likeCount = likeCount;
-        this.totalViewCount = totalViewCount;
-        this.isCompleted = isCompleted;
-        this.updatedAt = updatedAt;
-        this.createdAt = createdAt;
+    public NovelSimpleQueryProjection(Novel novel) {
+        this.novel = novel;
     }
 }

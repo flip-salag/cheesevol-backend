87063a9f0c118db7a3a4d1838fa98b9a08b0c600
Jaemin <ghdckdwn8456@gmail.com>
2025-12-16 18:15:34 +0900

    [feat] Episode 제목,설명,본문 최대/최소 길이 변경 - #48

--- novelservice/src/main/java/com/iucyh/novelservice/episode/constant/EpisodeConstants.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/constant/EpisodeConstants.java
@@ -2,11 +2,11 @@ package com.iucyh.novelservice.episode.constant;
 
 public abstract class EpisodeConstants {
 
-    public static final int EPISODE_TITLE_LENGTH_MIN = 1;
+    public static final int EPISODE_TITLE_LENGTH_MIN = 2;
     public static final int EPISODE_TITLE_LENGTH_MAX = 50;
 
-    public static final int EPISODE_DESC_LENGTH_MAX = 50;
+    public static final int EPISODE_DESC_LENGTH_MAX = 35;
 
-    public static final int EPISODE_CONTENT_LENGTH_MIN = 100;
+    public static final int EPISODE_CONTENT_LENGTH_MIN = 450;
     public static final int EPISODE_CONTENT_LENGTH_MAX = 20000;
 }

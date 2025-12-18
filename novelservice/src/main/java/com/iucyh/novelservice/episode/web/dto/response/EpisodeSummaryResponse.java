5637e4627cebaae3c2dce4583fc75ae224512c17
Jaemin <ghdckdwn8456@gmail.com>
2025-12-19 08:49:27 +0900

    [chore] EpisodeSummaryResponse->필드 일부 순서 변경 - #60

--- novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/response/EpisodeSummaryResponse.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/response/EpisodeSummaryResponse.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 public record EpisodeSummaryResponse(
 
         String episodeId,
+        int episodeNumber,
         String title,
         String description,
         int viewCount,
-        int episodeNumber,
         LocalDateTime createdAt
 ) {}

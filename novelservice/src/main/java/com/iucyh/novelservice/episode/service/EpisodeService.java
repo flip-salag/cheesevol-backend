622742a81be6cc04cf43e8f6d209f60f4fa031f3
Jaemin <ghdckdwn8456@gmail.com>
2025-12-18 12:42:32 +0900

    [chore] EpisodeService->주석 보완 - #56

--- novelservice/src/main/java/com/iucyh/novelservice/episode/service/EpisodeService.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/service/EpisodeService.java
@@ -33,13 +33,13 @@ public class EpisodeService {
     public EpisodeSummaryResponse createEpisode(CreateEpisodeCommand command) {
         Novel novel = findNovelWithUserId(command.userId(), command.novelPublicId());
         if (novel.isCompletedNovel()) {
-            throw new NovelAlreadyCompleted();
+            throw new NovelAlreadyCompleted(); // 완결된 소설은 회차 생성 불가
         }
 
         int newEpisodeNumber = novel.getLastEpisodeNumber() + 1;
         Episode episode = EpisodeCommandMapper.toEpisode(command, novel, newEpisodeNumber);
         Episode savedEpisode = episodeRepository.save(episode); // TODO: GlobalExceptionHandler에 DuplicateKeyException 핸들링 메서드 구현 (409)
-        novel.updateLastEpisode(savedEpisode.getEpisodeNumber(), savedEpisode.getCreatedAt());
+        novel.updateLastEpisode(savedEpisode.getEpisodeNumber(), savedEpisode.getCreatedAt()); // 소설의 마지막 회차와 관련된 컬럼들의 정합성을 위해 최신 회차 기준으로 업데이트
 
         return EpisodeResponseMapper.toEpisodeSummaryResponse(savedEpisode);
     }
@@ -61,12 +61,12 @@ public class EpisodeService {
         Novel novel = episode.getNovel();
 
         if (novel.isCompletedNovel()) {
-            throw new NovelAlreadyCompleted();
+            throw new NovelAlreadyCompleted(); // 완결된 소설은 회차 삭제 불가
         }
 
         episode.softDelete();
-        // 삭제될 회차를 제외하고 나머지 회차들 중 가장 최신회차의 등록일로 Novel의 lastEpisodeAt 업데이트
-        // 다음 최신 회차가 없다면 lastEpisodeAt을 null로 설정
+        // 삭제될 회차와 삭제된 회차를 제외하고 나머지 회차들 중 가장 최신회차의 등록일로 Novel의 lastEpisodeAt 업데이트
+        // 다음 최신 회차가 없다면 lastEpisodeAt을 null로 설정 (소설 목록 조회 시 소설의 lastEpisodeAt이 null이라면 회차가 없는 것으로 간주)
         LocalDateTime lastEpisodeAt = episodeQueryRepository.findLastEpisodeAtExceptDeletedEpisode(novel.getId(), episode.getPublicId());
         novel.updateLastEpisodeAt(lastEpisodeAt);
     }

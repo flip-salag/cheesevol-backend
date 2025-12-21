7af636fc49922f2581ce890d5a57a49c0293391f
Jaemin <ghdckdwn8456@gmail.com>
2025-12-21 14:17:03 +0900

    [chore] NovelPagingStrategyFactory->get 메서드에 문서 주석 추가 - #68

--- novelservice/src/main/java/com/iucyh/novelservice/novel/service/factory/NovelPagingStrategyFactory.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/service/factory/NovelPagingStrategyFactory.java
@@ -3,7 +3,6 @@ package com.iucyh.novelservice.novel.service.factory;
 import com.iucyh.novelservice.novel.enumtype.NovelSortType;
 import com.iucyh.novelservice.novel.repository.query.paging.NovelPagingStrategy;
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,7 +12,7 @@ import java.util.stream.Collectors;
 
 /**
  * <p>Novel의 페이징 전략을 조회하는 Factory</p>
- * <b>빈 생성 시 특정 NovelSortType과 매칭되는 전략이 없으면 에러 발생</b>
+ * <b>빈 생성 시 특정 NovelSortType과 매칭되는 전략이 없으면 {@code IllegalStateException} 발생</b>
  */
 @Component
 public class NovelPagingStrategyFactory {
@@ -40,6 +39,12 @@ public class NovelPagingStrategyFactory {
         }
     }
 
+    /**
+     * <p>{@code sortType}과 매칭되는 페이징 전략 조회</p>
+     * <b>빈 초기화 단계에서 각 {@code NovelSortType}과 매칭되는 전략이 있는지 검증하므로 null은 절대 반환되지 않음</b>
+     * @param sortType 조회 기준
+     * @return 전달된 {@code sortType}과 매칭되는 {@code NovelPagingStrategy}
+     */
     public NovelPagingStrategy get(NovelSortType sortType) {
         return pagingStrategyMap.get(sortType);
     }

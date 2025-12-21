f846ae1cc051837ce6ba8cd9573bc1e52d83ba7b
Jaemin <ghdckdwn8456@gmail.com>
2025-12-21 17:24:36 +0900

    [feat] NovelSummaryResponse 간소화 - #70

--- novelservice/src/main/java/com/iucyh/novelservice/novel/web/dto/response/NovelSummaryResponse.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/web/dto/response/NovelSummaryResponse.java
@@ -3,18 +3,12 @@ package com.iucyh.novelservice.novel.web.dto.response;
 import com.iucyh.novelservice.novel.enumtype.NovelCategory;
 import com.iucyh.novelservice.user.web.dto.response.info.UserBasicInfo;
 
-import java.time.LocalDateTime;
-
 public record NovelSummaryResponse(
 
         String novelId,
         UserBasicInfo author,
         String title,
-        String description,
         NovelCategory category,
-        int likeCount,
         int totalViewCount,
-        boolean isCompleted,
-        LocalDateTime updatedAt,
-        LocalDateTime createdAt
+        boolean isCompleted
 ) {}

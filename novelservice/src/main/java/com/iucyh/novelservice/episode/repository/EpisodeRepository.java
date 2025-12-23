2eaf3ead55bdb769fab982fc69b9ecea27b39eb4
Jaemin <ghdckdwn8456@gmail.com>
2025-12-23 15:13:42 +0900

    [pref] Episode 조회 쿼리에서 유저, 소설 삭제 여부 필터링 삭제 - #76

--- novelservice/src/main/java/com/iucyh/novelservice/episode/repository/EpisodeRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/repository/EpisodeRepository.java
@@ -20,7 +20,6 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
      * <p>{@code publicId}에 해당하는 회차를 조회하면서 아래 조건들을 동시에 검사</p>
      * <ul>
      *     <li>회차가 삭제되지 않았는지 여부</li>
-     *     <li>회차가 속한 소설이 삭제되지 않았는지 여부</li>
      *     <li>회차가 속한 소설의 작성자 id가 {@code userId}와 일치하는지 여부</li>
      * </ul>
      */
@@ -29,8 +28,9 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
     from Episode e
     join e.novel n
     where
-        e.publicId = :publicId and e.deletedAt is null
-        and n.user.id = :userId and n.deletedAt is null
+        e.publicId = :publicId
+        and e.deletedAt is null
+        and n.user.id = :userId
     """)
     Optional<Episode> findByPublicIdWithNovelUser(@Param("publicId") String publicId, @Param("userId") Long userId);
 
@@ -38,7 +38,6 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
      * <p>{@code publicId}에 해당하는 회차를 조회하면서 아래 조건들을 동시에 검사</p>
      * <ul>
      *     <li>회차가 삭제되지 않았는지 여부</li>
-     *     <li>회차가 속한 소설이 삭제되지 않았는지 여부</li>
      *     <li>회차가 속한 소설의 작성자 id가 {@code userId}와 일치하는지 여부</li>
      * </ul>
      * <b>주의: 조회 시 Novel 엔티티도 같이 조회 (fetch join), 이후 로직에서 Novel 엔티티가 필요 없다면 Fetch 가 붙지 않은 메서드 사용 권장</b>
@@ -48,8 +47,9 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
     from Episode e
     join fetch e.novel n
     where
-        e.publicId = :publicId and e.deletedAt is null
-        and n.user.id = :userId and n.deletedAt is null
+        e.publicId = :publicId
+        and e.deletedAt is null
+        and n.user.id = :userId
     """)
     Optional<Episode> findByPublicIdWithNovelUserFetch(@Param("publicId") String publicId, @Param("userId") Long userId);
 
@@ -58,7 +58,8 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
     void increaseViewCount(@Param("episodeId") Long episodeId);
 
     /**
-     * 특정 Novel에 속하는 회차들을 bulk update 로 soft delete 처리
+     * <p>특정 Novel에 속하는 회차들을 bulk update 로 soft delete 처리</p>
+     * <b>이미 삭제된 회차들은 무시</b>
      * @param novelId 삭제할 회차들이 속한 Novel의 pk
      * @param deletedAt 각 Episode에 기록될 삭제 시간, 가급적 현재 시간 혹은 부모 Novel의 삭제 시간 전달 권장
      */
@@ -66,7 +67,7 @@ public interface EpisodeRepository extends PublicEntityRepository<Episode, Long>
     @Query("""
     update Episode e
     set e.deletedAt = :deletedAt
-    where e.novel.id = :novelId
+    where e.novel.id = :novelId and e.deletedAt is null
     """)
     void softDeleteByNovelId(Long novelId, LocalDateTime deletedAt);
 }
--- novelservice/src/main/java/com/iucyh/novelservice/episode/repository/query/EpisodeQueryRepositoryImpl.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/repository/query/EpisodeQueryRepositoryImpl.java
@@ -111,7 +111,7 @@ public class EpisodeQueryRepositoryImpl implements EpisodeQueryRepository {
                 .join(novel.user, user)
                 .where(
                         episode.publicId.eq(publicId),
-                        applyValidEpisodeFilter()
+                        episode.deletedAt.isNull()
                 )
                 .fetchOne();
         return Optional.ofNullable(result);
@@ -122,11 +122,9 @@ public class EpisodeQueryRepositoryImpl implements EpisodeQueryRepository {
         String result = queryFactory
                 .select(episode.content)
                 .from(episode)
-                .join(episode.novel, novel)
-                .join(novel.user, user)
                 .where(
                         episode.publicId.eq(publicId),
-                        applyValidEpisodeFilter()
+                        episode.deletedAt.isNull()
                 )
                 .fetchOne();
         return Optional.ofNullable(result);
@@ -172,21 +170,5 @@ public class EpisodeQueryRepositoryImpl implements EpisodeQueryRepository {
 
     private OrderSpecifier<Integer> applyOrder(EpisodeSortType sortType) {
         return sortType == EpisodeSortType.ASC ? episode.episodeNumber.asc() : episode.episodeNumber.desc();
-    }
-
-    /**
-     * <p>조회할 회차가 유효한 회차인지 검사하기 위한 필터 적용</p>
-     * <p>필터 조건</p>
-     * <ul>
-     *     <li>삭제되지 않은 회차</li>
-     *     <li>회차가 속한 소설이 삭제되지 않은 상태</li>
-     *     <li>회차가 속한 소설의 작성자가 삭제되지 않은 상태</li>
-     * </ul>
-     * <b>주의: novel, user와 관련된 조건을 사용하므로 쿼리에서 novel, user JOIN 필수</b>
-     */
-    private BooleanExpression applyValidEpisodeFilter() {
-        return episode.deletedAt.isNull()
-                .and(novel.deletedAt.isNull())
-                .and(user.deletedAt.isNull());
     }
 }

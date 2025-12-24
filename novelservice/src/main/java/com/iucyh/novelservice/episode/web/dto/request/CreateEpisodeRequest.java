4e47521837958424dbddbe91aa0c97004e08d6f9
Jaemin <ghdckdwn8456@gmail.com>
2025-12-24 17:17:24 +0900

    [feat] Episode 생성, Episode 본문 수정 Request에 html 관련 검증 적용 - #78

--- novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/CreateEpisodeRequest.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/CreateEpisodeRequest.java
@@ -1,5 +1,10 @@
 package com.iucyh.novelservice.episode.web.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.iucyh.novelservice.common.deserializer.html.HtmlDeserializer;
+import com.iucyh.novelservice.common.deserializer.html.HtmlSanitized;
+import com.iucyh.novelservice.common.util.htmlsanitizer.HtmlContentType;
+import com.iucyh.novelservice.common.validator.html.SizeWithoutHtml;
 import com.iucyh.novelservice.episode.constant.EpisodeConstants;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -18,6 +23,8 @@ public record CreateEpisodeRequest(
         String description,
 
         @NotBlank
-        @Size(min = EPISODE_CONTENT_LENGTH_MIN, max = EPISODE_CONTENT_LENGTH_MAX)
+        @SizeWithoutHtml(min = EPISODE_CONTENT_LENGTH_MIN, max = EPISODE_CONTENT_LENGTH_MAX)
+        @JsonDeserialize(using = HtmlDeserializer.class)
+        @HtmlSanitized(HtmlContentType.EPISODE_CONTENT)
         String content
 ) {}

475bea5dc2965f8785f471beef7a11ee671c09c9
Jaemin <ghdckdwn8456@gmail.com>
2025-12-24 16:43:56 +0900

    [feat] SizeWithoutHtml validator 구현 - #78

---
+++ novelservice/src/main/java/com/iucyh/novelservice/common/validator/html/SizeWithoutHtmlValidator.java
@@ -0,0 +1,27 @@
+package com.iucyh.novelservice.common.validator.html;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.jsoup.Jsoup;
+
+public class SizeWithoutHtmlValidator implements ConstraintValidator<SizeWithoutHtml, String> {
+
+    private int min;
+    private int max;
+
+    @Override
+    public void initialize(SizeWithoutHtml constraintAnnotation) {
+        this.min = constraintAnnotation.min();
+        this.max = constraintAnnotation.max();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        String parsedText = Jsoup.parse(value).wholeText();
+        return parsedText.length() >= min && parsedText.length() <= max;
+    }
+}

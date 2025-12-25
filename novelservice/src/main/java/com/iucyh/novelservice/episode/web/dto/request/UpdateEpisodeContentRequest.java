636c52aaf27461c3ba1cf42a132d873114d2d853
Jaemin <ghdckdwn8456@gmail.com>
2025-12-25 13:50:20 +0900

    [chore] UpdateEpisodeContentRequest->content 필드 NotBlank 검증으로 변경

--- novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/UpdateEpisodeContentRequest.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/UpdateEpisodeContentRequest.java
@@ -5,14 +5,14 @@ import com.iucyh.novelservice.common.deserializer.html.HtmlDeserializer;
 import com.iucyh.novelservice.common.deserializer.html.HtmlSanitized;
 import com.iucyh.novelservice.common.util.htmlsanitizer.HtmlContentType;
 import com.iucyh.novelservice.common.validator.htmlsize.SizeWithoutHtml;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 import static com.iucyh.novelservice.episode.constant.EpisodeConstants.EPISODE_CONTENT_LENGTH_MAX;
 import static com.iucyh.novelservice.episode.constant.EpisodeConstants.EPISODE_CONTENT_LENGTH_MIN;
 
 public record UpdateEpisodeContentRequest(
 
-        @NotNull
+        @NotBlank
         @SizeWithoutHtml(min = EPISODE_CONTENT_LENGTH_MIN, max = EPISODE_CONTENT_LENGTH_MAX)
         @JsonDeserialize(using = HtmlDeserializer.class)
         @HtmlSanitized(HtmlContentType.EPISODE_CONTENT)

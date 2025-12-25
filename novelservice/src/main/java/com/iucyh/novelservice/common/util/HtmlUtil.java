b9adb2b4a514f929be1041a156e9c79a066fba82
Jaemin <ghdckdwn8456@gmail.com>
2025-12-25 23:55:49 +0900

    [feat] HtmlSanitizerUtil -> HtmlUtil 로 통합 - #81

--- novelservice/src/main/java/com/iucyh/novelservice/common/util/HtmlUtil.java
+++ novelservice/src/main/java/com/iucyh/novelservice/common/util/HtmlUtil.java
@@ -1,6 +1,7 @@
-package com.iucyh.novelservice.common.util.html;
+package com.iucyh.novelservice.common.util;
 
 import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 
 /**
  * <p>HTML과 관련된 편의 메서드를 제공하는 유틸리티</p>
@@ -8,6 +9,25 @@ import org.jsoup.Jsoup;
 public class HtmlUtil {
 
     private HtmlUtil() {}
+
+    /**
+     * <p>전달된 HTML 문자열을 정제</p>
+     * <p>전달된 문자열이 비어있거나, {@code null}이라면 그 값 그대로 반환</p>
+     * @param safelist HTML 정제 시 사용할 white list
+     * @param html 정제할 문자열
+     * @return 규칙에 맞게 정제된 HTML 문자열
+     */
+    public static String sanitize(Safelist safelist, String html) {
+        if (html == null) {
+            return null;
+        }
+
+        if (html.isBlank()) {
+            return "";
+        }
+
+        return Jsoup.clean(html, safelist);
+    }
 
     /**
      * <p>모든 HTML 태그를 제거한 순수 택스트를 반환, new line(\n), 공백은 유지</p>

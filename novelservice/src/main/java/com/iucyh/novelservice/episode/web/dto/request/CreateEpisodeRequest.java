cc563a06d73d3c2d32725dfde41e48b62eb31e3e
Jaemin <ghdckdwn8456@gmail.com>
2025-12-27 13:27:48 +0900

    [feat] CreateEpisodeRequest -> episodeType 필드 추가 - #80

--- novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/CreateEpisodeRequest.java
+++ novelservice/src/main/java/com/iucyh/novelservice/episode/web/dto/request/CreateEpisodeRequest.java
@@ -1,10 +1,12 @@
 package com.iucyh.novelservice.episode.web.dto.request;
 
 import com.iucyh.novelservice.common.deserializer.html.SanitizedHtml;
+import com.iucyh.novelservice.common.validator.enumfield.EnumField;
 import com.iucyh.novelservice.common.validator.htmlnotblank.NotBlankWithoutHtml;
 import com.iucyh.novelservice.common.validator.htmlsize.SizeWithoutHtml;
 import com.iucyh.novelservice.common.vo.HtmlContent;
 import com.iucyh.novelservice.episode.constant.EpisodeConstants;
+import com.iucyh.novelservice.episode.enumtype.EpisodeType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,6 +14,10 @@ import jakarta.validation.constraints.Size;
 import static com.iucyh.novelservice.episode.constant.EpisodeConstants.*;
 
 public record CreateEpisodeRequest(
+
+        @NotNull
+        @EnumField(enumClass = EpisodeType.class)
+        String episodeType,
 
         @NotBlank
         @Size(min = EPISODE_TITLE_LENGTH_MIN, max = EpisodeConstants.EPISODE_TITLE_LENGTH_MAX)

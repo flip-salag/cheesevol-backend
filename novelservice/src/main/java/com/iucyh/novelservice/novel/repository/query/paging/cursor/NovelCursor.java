a57491f3ec1458563db1b6ed9035b4920b8b665c
Jaemin <ghdckdwn8456@gmail.com>
2026-01-06 22:10:28 +0900

    [chore] NovelCursor -> 상속에서 구현으로 표현 변경 - #105

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelCursor.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelCursor.java
@@ -2,7 +2,7 @@ package com.iucyh.novelservice.novel.repository.query.paging.cursor;
 
 /**
  * <p>Novel의 페이징에서 사용되는 커서들의 기본 타입</p>
- * <b>모든 페이징 커서 record들은 이 인터페이스를 상속해야 합니다.</b>
+ * <b>모든 페이징 커서 record들은 이 인터페이스를 구현해야 합니다.</b>
  */
 public interface NovelCursor {
 }

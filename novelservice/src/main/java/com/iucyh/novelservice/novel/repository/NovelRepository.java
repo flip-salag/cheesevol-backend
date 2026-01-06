23f89a56f23431786096f96aec67dc200922048b
Jaemin <ghdckdwn8456@gmail.com>
2026-01-06 22:29:44 +0900

    [chore] NovelRepository -> 쓰이지 않는 메서드 삭제 - #105

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/NovelRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/NovelRepository.java
@@ -32,19 +32,6 @@ public interface NovelRepository extends JpaRepository<Novel, Long> {
     """)
     Optional<Novel> findByPublicIdFetch(@Param("publicId") String publicId);
 
-    /**
-     * <p>특정 작가의 소설 중 전달된 title과 중복되는 제목이 존재하는 지 검사</p>
-     * <b>Soft Delete 된 소설을 포함하여 검사</b>
-     */
-    boolean existsByTitleAndUserId(String title, Long userId);
-
-    /**
-     * <p>특정 작가의 소설 중 전달된 publicId 에 해당하는 소설을 제외하고
-     * 나머지 소설들 중에서 전달된 title과 중복되는 제목이 존재하는 지 검사</p>
-     * <b>Soft Delete 된 소설을 포함하여 검사</b>
-     */
-    boolean existsByTitleAndUserIdAndPublicIdNot(String title, Long userId, String publicId);
-
     @Modifying(clearAutomatically = true)
     @Query("update Novel n set n.totalViewCount = n.totalViewCount + 1 where n.id = :novelId and n.deletedAt is null")
     void increaseTotalViewCount(@Param("novelId") Long novelId);

a71a60ff03e836db5ab2158e9a16123fb7a04589
Jaemin <ghdckdwn8456@gmail.com>
2026-01-06 22:32:10 +0900

    [chore] NovelPagingStrategy -> createCursor 메서드 주석 일부 수정 - #105ㅋ

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelPagingStrategy.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelPagingStrategy.java
@@ -20,7 +20,7 @@ public interface NovelPagingStrategy {
 
     /**
      * <p>해당 전략이 담당하는 정렬 기준에 맞는 {@code NovelCursor}를 생성하는 메서드</p>
-     * <b>주의: 조회 시 사용한 전략을 그대로 사용해야 합니다. 다른 전략을 임의로 생성하거나 찾아서 쓸 경우 런타임 에러가 발생할 수 있습니다.</b>
+     * <b>주의: 조회 시 사용한 전략을 그대로 사용해야 합니다. 다른 전략을 임의로 생성하거나 찾아서 쓸 경우 의도치 않은 동작이 발생할 수 있습니다.</b>
      * @param lastResult 조회된 결과의 마지막 요소
      * @return 생성된 {@code NovelCursor}
      */

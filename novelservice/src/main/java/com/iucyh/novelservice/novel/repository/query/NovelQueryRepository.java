2b6324aaa20da53a06aa51f6632fdf56e84e2bef
Jaemin <ghdckdwn8456@gmail.com>
2026-01-06 22:40:07 +0900

    [chore] NovelQueryRepository -> NovelPagingCondition 인자 주석 추가 - #105

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepository.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/NovelQueryRepository.java
@@ -33,6 +33,7 @@ public interface NovelQueryRepository {
     /**
      * <p>Novel 목록 조회, category가 null이 아닐 시 해당 category에 해당하는 Novel만 조회</p>
      * <p>삭제되었거나(soft delete 포함), COMMON(일반) 회차가 한개도 존재하지 않는 소설은 제외</p>
+     * @param condition 페이징 조건 (커서, limit 등)
      * @param strategy Novel의 정렬 기준에 따른 페이징 쿼리 생성 전략
      * @param category 필터링 할 카테고리, 모든 카테고리 조회 시 null 전달
      */
@@ -41,6 +42,7 @@ public interface NovelQueryRepository {
     /**
      * <p>이번달에 생성된 신작 Novel 목록 조회, category가 null이 아닐 시 해당 category에 해당하는 Novel만 조회</p>
      * <p>삭제되었거나(soft delete 포함), COMMON(일반) 회차가 한개도 존재하지 않는 소설은 제외</p>
+     * @param condition 페이징 조건 (커서, limit 등)
      * @param strategy Novel의 정렬 기준에 따른 페이징 쿼리 생성 전략
      * @param category 필터링 할 카테고리, 모든 카테고리 조회 시 null 전달
      */

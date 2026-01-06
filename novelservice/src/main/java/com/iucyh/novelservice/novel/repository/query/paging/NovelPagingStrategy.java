0980b66aaa6a50ae1236291d93687c9d7e6680aa
Jaemin <ghdckdwn8456@gmail.com>
2026-01-06 22:18:14 +0900

    [chore] NovelPagingStrategy -> 각 메서드 주석 추가 - #105

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelPagingStrategy.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/NovelPagingStrategy.java
@@ -5,6 +5,9 @@ import com.iucyh.novelservice.novel.repository.query.paging.cursor.NovelCursor;
 import com.iucyh.novelservice.novel.enumtype.NovelSortType;
 import com.querydsl.jpa.impl.JPAQuery;
 
+/**
+ * <p>Novel의 각 정렬 기준에 맞는 페이징 쿼리 생성을 위한 전략</p>
+ */
 public interface NovelPagingStrategy {
 
     /**
@@ -14,6 +17,17 @@ public interface NovelPagingStrategy {
      * @return 페이징이 적용된 쿼리
      */
     JPAQuery<Novel> applyPaging(JPAQuery<Novel> query, NovelCursor cursor);
+
+    /**
+     * <p>해당 전략이 담당하는 정렬 기준에 맞는 {@code NovelCursor}를 생성하는 메서드</p>
+     * <b>주의: 조회 시 사용한 전략을 그대로 사용해야 합니다. 다른 전략을 임의로 생성하거나 찾아서 쓸 경우 런타임 에러가 발생할 수 있습니다.</b>
+     * @param lastResult 조회된 결과의 마지막 요소
+     * @return 생성된 {@code NovelCursor}
+     */
     NovelCursor createCursor(Novel lastResult);
+
+    /**
+     * @return 해당 전략이 담당하는 정렬 기준과 매핑되는 {@code NovelSortType}
+     */
     NovelSortType getSupportedSortType();
 }

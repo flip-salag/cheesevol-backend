cb5f4dbaf09a4dfe9596a634a09004648b8097f4
Jaemin <ghdckdwn8456@gmail.com>
2026-01-14 22:51:30 +0900

    [feat] 소설 목록 조회 좋아요순 정렬 기준 개편 - #132

--- novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelLikeCountCursor.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/repository/query/paging/cursor/NovelLikeCountCursor.java
@@ -1,10 +1,8 @@
 package com.iucyh.novelservice.novel.repository.query.paging.cursor;
 
-import java.time.LocalDate;
-
 public record NovelLikeCountCursor(
 
-        long lastNovelId,
-        int lastLikeCount,
-        LocalDate lastEpisodePublishDate
+        long novelId,
+        int likeCount,
+        int totalViewCount
 ) implements NovelCursor {}

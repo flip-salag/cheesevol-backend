9fcccd2fa0d6a04590a13e9dc40a54bfe2b91a69
Jaemin <ghdckdwn8456@gmail.com>
2026-01-18 07:01:48 +0900

    [chore] NovelPolicyValidator -> 문서 주석 일부 수정 - #144

--- novelservice/src/main/java/com/iucyh/novelservice/novel/service/policy/NovelPolicyValidator.java
+++ novelservice/src/main/java/com/iucyh/novelservice/novel/service/policy/NovelPolicyValidator.java
@@ -19,9 +19,9 @@ public class NovelPolicyValidator {
 
     /**
      * <p>{@code cursor}가 반환하는 {@code NovelSortType}과 전달된 {@code sortType}이 일치하는지 검증</p>
-     * @param cursor {@code NovelCursor} 객체로 디코딩 된 클라이언트로부터 전달받은 커서
+     * @param cursor {@code NovelCursor} 객체로 디코딩된 클라이언트로부터 전달받은 커서
      * @param sortType 클라이언트로부터 전달받은 정렬 기준
-     * @throws InvalidNovelCursor 커서가 해당 정렬기준과 맞지 않을 때
+     * @throws InvalidNovelCursor 커서가 해당 정렬 기준과 맞지 않을 때
      */
     public void validateNovelCursorMatchesSortType(NovelCursor cursor, NovelSortType sortType) throws InvalidNovelCursor {
         if (cursor.getSortType() != sortType) {

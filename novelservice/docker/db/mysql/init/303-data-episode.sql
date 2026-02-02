916e1ee99f12b282b6f85f99519d5a382f5c1a20
Jaemin <ghdckdwn8456@gmail.com>
2026-02-02 18:07:43 +0900

    [feat] episode 로컬 더미 데이터 생성 스크립트 개선 및 발행일 추가 - #152

--- novelservice/docker/db/mysql/init/303-data-episode.sql
+++ novelservice/docker/db/mysql/init/303-data-episode.sql
@@ -1,7 +1,22 @@
 USE `novel_service`;
 
+/*
+==== 소설 당 회차 개수 ====
+프롤로그가 존재하는 소설의 회차 개수 : 6개 (프롤로그 1개 + 일반 회차 5개)
+프롤로그가 존재하지 않는 소설의 회차 개수 : 5개 (일반 회차 5개)
+
+==== 회차 분포 ====
+프롤로그가 존재하는 소설 개수 : 4개
+프롤로그가 존재하지 않는 소설 개수(소설3, 소설4, 소설6) : 3개
+
+삭제된 회차 개수(소설4, 소설5, 소설6의 회차번호 3번) : 3개 (+ 소설4, 소설8의 모든 회차들, 해당 소설들은 소설 자체가 삭제되었으므로 회차도 같이 삭제)
+
+발행일 : 소설의 발행일 + 회차번호 + 2일 (각 회차마다 1일씩 간격을 줌)
+생성일 : 소설의 생성일 + 회차번호 + 2일 (각 회차마다 1일씩 간격을 줌)
+수정일 : 소설의 수정일 + 회차번호 + 2일 (각 회차마다 1일씩 간격을 줌)
+*/
+
 SET @novel1 := (SELECT novel_id FROM novel WHERE title = 'novel-1');
-SET @novel2 := (SELECT novel_id FROM novel WHERE title = 'novel-2');
 SET @novel3 := (SELECT novel_id FROM novel WHERE title = 'novel-3');
 SET @novel4 := (SELECT novel_id FROM novel WHERE title = 'novel-4');
 SET @novel5 := (SELECT novel_id FROM novel WHERE title = 'novel-5');
@@ -9,12 +24,12 @@ SET @novel6 := (SELECT novel_id FROM novel WHERE title = 'novel-6');
 SET @novel7 := (SELECT novel_id FROM novel WHERE title = 'novel-7');
 SET @novel8 := (SELECT novel_id FROM novel WHERE title = 'novel-8');
 
-DROP TEMPORARY TABLE IF EXISTS temp_episode_seq;
-CREATE TEMPORARY TABLE temp_episode_seq (ep_no INT NOT NULL);
+DROP TABLE IF EXISTS temp_episode_seq;
+CREATE TABLE temp_episode_seq (ep_no INT NOT NULL);
 INSERT INTO temp_episode_seq VALUES (0), (1), (2), (3), (4), (5);
 
+INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, published_at, deleted_at)
 -- ==== novel 1 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
@@ -24,6 +39,7 @@ SELECT
     REPEAT('a', 5000),
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
@@ -39,53 +55,23 @@ JOIN (
     UNION ALL
     SELECT 5, '9VgtrkaKNZC5vk9rvQtfO'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel1;
-
--- ==== novel 2 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
-SELECT
-    n.novel_id,
-    pid.public_id,
-    CASE WHEN e.ep_no = 0 THEN 'PROLOGUE' ELSE 'COMMON' END,
-    CONCAT(n.novel_id, '_episode_', e.ep_no),
-    'dummy episode',
-    REPEAT('a', 5000),
-    e.ep_no,
-    1000 + (e.ep_no * 5),
-    n.deleted_at
-FROM temp_episode_seq e
-JOIN (
-    SELECT 0 AS rn, 'InD3qvD_naBdY6BLgtJyo' AS public_id
-    UNION ALL
-    SELECT 1, 'Nn8vqEq8VNezIAE11FZ_C'
-    UNION ALL
-    SELECT 2, 'vR1-QANWW1QvxvfM1e81U'
-    UNION ALL
-    SELECT 3, '2XZBM8vDapnSKFMGQqD4w'
-    UNION ALL
-    SELECT 4, 'MYPWuzHzlREht_lYIylvt'
-    UNION ALL
-    SELECT 5, 'D0R5hFC5y4CZeqcy5v8TB'
-) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel2;
-
+JOIN novel n ON n.novel_id = @novel1
+UNION ALL
 -- ==== novel 3 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
-    CASE WHEN e.ep_no = 0 THEN 'PROLOGUE' ELSE 'COMMON' END,
+    'COMMON',
     CONCAT(n.novel_id, '_episode_', e.ep_no),
     'dummy episode',
     REPEAT('a', 5000),
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
-    SELECT 0 AS rn, 'cJkfEXh4Av0SKG_uHMi3_' AS public_id
-    UNION ALL
-    SELECT 1, 'XE1QLt6YC81sE0OxIZl7m'
+    SELECT 1 AS rn, 'XE1QLt6YC81sE0OxIZl7m' AS public_id
     UNION ALL
     SELECT 2, 'Vqc_98bLn5qnnfaFa9xQL'
     UNION ALL
@@ -95,25 +81,24 @@ JOIN (
     UNION ALL
     SELECT 5, '0BSj9fMtwJ2-vBQeeKXJY'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel3;
-
+JOIN novel n ON n.novel_id = @novel3
+WHERE e.ep_no > 0
+UNION ALL
 -- ==== novel 4 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
-    CASE WHEN e.ep_no = 0 THEN 'PROLOGUE' ELSE 'COMMON' END,
+    'COMMON',
     CONCAT(n.novel_id, '_episode_', e.ep_no),
     'dummy episode',
     REPEAT('a', 5000),
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
-    SELECT 0 AS rn, '-B6HzHQ-1ceMSz_NFfuBz' AS public_id
-    UNION ALL
-    SELECT 1, 'IP10wNhIWZaCJ_nu8SIx0'
+    SELECT 1 AS rn, 'IP10wNhIWZaCJ_nu8SIx0' AS public_id
     UNION ALL
     SELECT 2, 'hbR_sd6swYKWxAXR9plU-'
     UNION ALL
@@ -123,10 +108,10 @@ JOIN (
     UNION ALL
     SELECT 5, '2tenHAmhA9EIw37zif1eT'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel4;
-
+JOIN novel n ON n.novel_id = @novel4
+WHERE e.ep_no > 0
+UNION ALL
 -- ==== novel 5 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
@@ -136,6 +121,7 @@ SELECT
     CASE WHEN e.ep_no < 4 THEN REPEAT('a', 5000) ELSE REPEAT('a', 20000) END,
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
@@ -151,25 +137,23 @@ JOIN (
     UNION ALL
     SELECT 5, 'LomE6_voKK3t-hz6Xe_C-'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel5;
-
+JOIN novel n ON n.novel_id = @novel5
+UNION ALL
 -- ==== novel 6 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
-    CASE WHEN e.ep_no = 0 THEN 'PROLOGUE' ELSE 'COMMON' END,
+    'COMMON',
     CONCAT(n.novel_id, '_episode_', e.ep_no),
     'dummy episode',
     CASE WHEN e.ep_no < 4 THEN REPEAT('a', 5000) ELSE REPEAT('a', 20000) END,
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
-    SELECT 0 AS rn, 'fSmdtM3QEzCIJRvHZjkmA' AS public_id
-    UNION ALL
-    SELECT 1, 'IKzzxFamAP2pe8rmX9toR'
+    SELECT 1 AS rn, 'IKzzxFamAP2pe8rmX9toR' AS public_id
     UNION ALL
     SELECT 2, 'O5O72NpyX0R99_Dv2pWZ6'
     UNION ALL
@@ -179,10 +163,10 @@ JOIN (
     UNION ALL
     SELECT 5, '1IfAlOrE9-njbno-NEiyG'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel6;
-
+JOIN novel n ON n.novel_id = @novel6
+WHERE e.ep_no > 0
+UNION ALL
 -- ==== novel 7 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
@@ -192,6 +176,7 @@ SELECT
     CASE WHEN e.ep_no < 4 THEN REPEAT('a', 5000) ELSE REPEAT('a', 20000) END,
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
@@ -207,10 +192,9 @@ JOIN (
     UNION ALL
     SELECT 5, '8Jmhkd4Z1LUKDrFKbXCkj'
 ) pid ON pid.rn = e.ep_no
-JOIN novel n ON n.novel_id = @novel7;
-
+JOIN novel n ON n.novel_id = @novel7
+UNION ALL
 -- ==== novel 8 ====
-INSERT INTO episode (novel_id, public_id, episode_type, title, description, content, episode_number, view_count, deleted_at)
 SELECT
     n.novel_id,
     pid.public_id,
@@ -220,6 +204,7 @@ SELECT
     CASE WHEN e.ep_no < 4 THEN REPEAT('a', 5000) ELSE REPEAT('a', 20000) END,
     e.ep_no,
     1000 + (e.ep_no * 5),
+    DATE_ADD(n.published_at, INTERVAL (e.ep_no + 2) DAY),
     n.deleted_at
 FROM temp_episode_seq e
 JOIN (
@@ -242,10 +227,17 @@ UPDATE episode e
 JOIN novel n ON n.novel_id = e.novel_id
 SET
     e.created_at = DATE_ADD(n.created_at, INTERVAL (e.episode_number + 2) DAY),
-    e.updated_at = DATE_ADD(n.created_at, INTERVAL (e.episode_number + 2) DAY);
+    e.updated_at = DATE_ADD(n.updated_at, INTERVAL (e.episode_number + 2) DAY);
 
 UPDATE episode
 SET deleted_at = NOW()
-WHERE episode_number = 3 AND novel_id IN (@novel4, @novel5);
+WHERE episode_number = 3 AND novel_id IN (@novel4, @novel5, @novel6);
 
-DROP TEMPORARY TABLE IF EXISTS temp_episode_seq;
+UPDATE novel n
+JOIN episode e ON e.novel_id = n.novel_id AND e.episode_number = 5
+SET
+    common_episode_count = 5,
+    max_episode_number = 5,
+    last_episode_publish_date = DATE(e.published_at);
+
+DROP TABLE IF EXISTS temp_episode_seq;

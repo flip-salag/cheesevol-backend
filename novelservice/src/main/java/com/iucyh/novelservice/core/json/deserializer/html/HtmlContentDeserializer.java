3421f40887b30c8480ea89b9adf41c8aa9b1d366
Jaemin <ghdckdwn8456@gmail.com>
2026-03-03 13:57:55 +0900

    [feat] HtmlContent 역직렬화기 예외 로직 개선 및 클래스 레벨에서도 사용가능하도록 수정 - #181

--- novelservice/src/main/java/com/iucyh/novelservice/core/json/deserializer/html/HtmlContentDeserializer.java
+++ novelservice/src/main/java/com/iucyh/novelservice/core/json/deserializer/html/HtmlContentDeserializer.java
@@ -2,12 +2,14 @@ package com.iucyh.novelservice.core.json.deserializer.html;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.iucyh.novelservice.core.json.deserializer.html.registry.SafelistRegistry;
 import com.iucyh.novelservice.common.vo.HtmlContent;
 import org.jsoup.safety.Safelist;
@@ -18,18 +20,17 @@ import java.io.IOException;
 
 /**
  * <p>JSON의 HTML 문자열 필드를 내부에서 사용하는 HtmlContent 타입으로 변환</p>
- * <b>@SanitizedHtml 어노테이션을 필드에 붙이지 않거나 Safelist를 조회하기 위한 Key가 유효하지 않다면 {@code IllegalStateException} 예외 발생</b>
+ * <b>@SanitizedHtml 어노테이션을 찾을 수 없거나 Safelist를 조회하기 위한 Key가 유효하지 않다면 실행 중 예외 발생 가능</b>
  */
 @JsonComponent
 public class HtmlContentDeserializer extends StdDeserializer<HtmlContent> implements ContextualDeserializer {
 
     private final SafelistRegistry registry; // Safelist 정책을 조회하기 위한 전용 Registry
-    private String safelistKey; // Safelist 정책을 조회하기 위한 Key
+    private final String safelistKey; // Safelist 정책을 조회하기 위한 Key
 
     @Autowired
     public HtmlContentDeserializer(SafelistRegistry registry) {
-        super(HtmlContent.class);
-        this.registry = registry;
+        this(registry, null);
     }
 
     private HtmlContentDeserializer(SafelistRegistry registry, String safelistKey) {
@@ -41,17 +42,15 @@ public class HtmlContentDeserializer extends StdDeserializer<HtmlContent> implem
     @Override
     public JsonDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property) throws JsonMappingException {
         if (property == null) {
-            return new HtmlContentDeserializer(registry);
+            return this;
         }
 
-        // 개발자의 실수와 관련된 예외이므로 Jackson 예외가 아니라 IllegalStateException을 던져 더 명확하게 구분
-        SanitizedHtml annotation = property.getAnnotation(SanitizedHtml.class);
-        if (annotation == null) { // 필드에 필수 어노테이션이 없다면
-            throw new IllegalStateException("@SanitizedHtml is required on property %s".formatted(property.getName()));
-        }
-
+        SanitizedHtml annotation = getSanitizedHtmlAnnotation(ctxt, property);
         if (!registry.containsKey(annotation.value())) { // 조회에 사용할 Key가 유효하지 않다면
-            throw new IllegalStateException("Safelist with key %s is not registered".formatted(annotation.value()));
+            return ctxt.reportBadDefinition(
+                    HtmlContent.class,
+                    "Safelist with key %s is not registered".formatted(annotation.value())
+            );
         }
 
         return new HtmlContentDeserializer(registry, annotation.value());
@@ -59,20 +58,50 @@ public class HtmlContentDeserializer extends StdDeserializer<HtmlContent> implem
 
     @Override
     public HtmlContent deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        String value = p.getValueAsString();
-
-        if (value == null) {
-            return null;
-        }
-
         if (safelistKey == null) {
             return ctxt.reportBadDefinition(
                     HtmlContent.class,
-                    "HtmlContent cannot be deserialized (property is null)"
+                    "HtmlContent cannot be deserialized. (safelistKey is null)"
             );
         }
 
+        JsonToken token = p.currentToken();
+        if (token == JsonToken.VALUE_NULL) { // 필드의 값이 null이라면 그대로 null 반환
+            return null;
+        }
+
+        if (token != JsonToken.VALUE_STRING) {
+            return ctxt.reportInputMismatch(
+                    HtmlContent.class,
+                    "HtmlContent only supports String type, current type: %s", token
+            );
+        }
+
+        String value = p.getValueAsString();
         Safelist safelist = registry.getSafelist(safelistKey);
         return HtmlContent.of(value, safelist);
+    }
+
+    /**
+     * <p>주어진 {@code property}를 이용해 {@code SanitizedHtml} 어노테이션 추출</p>
+     * @param ctxt 현재 메서드를 호출한 곳에서 사용하고 있는 {@code DeserializationContext}
+     * @param property 어노테이션을 추출할 대상
+     * @return 추출한 {@code SanitizedHtml} 어노테이션
+     * @throws JsonMappingException 필드 혹은 필드의 상위(필드가 소속된 클래스 등)에 어노테이션이 없을 때
+     */
+    private SanitizedHtml getSanitizedHtmlAnnotation(DeserializationContext ctxt, BeanProperty property) throws JsonMappingException {
+        SanitizedHtml annotation = property.getAnnotation(SanitizedHtml.class);
+        if (annotation == null) { // 필드에 필수 어노테이션이 없다면
+            annotation = property.getContextAnnotation(SanitizedHtml.class);
+            if (annotation == null) { // 필수 어노테이션이 상위(필드가 소속된 클래스 등)에도 없다면 예외 발생
+                AnnotatedMember member = property.getMember();
+                String exceptionMessage = "@SanitizedHtml is required on property '%s' in %s"
+                        .formatted(property.getName(), member != null ? member.getDeclaringClass().getName() : "unknown");
+
+                return ctxt.reportBadDefinition(HtmlContent.class, exceptionMessage);
+            }
+        }
+
+        return annotation;
     }
 }

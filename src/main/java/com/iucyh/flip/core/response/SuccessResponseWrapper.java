f343d6542e04f3eba5a0accdf9f9cb1c02a51ae6
Jaemin <ghdckdwn8456@gmail.com>
2026-08-02 19:03:49 +0900

    chore: SuccessResponseWrapper 주석 일부 표현 수정

--- src/main/java/com/iucyh/flip/core/response/SuccessResponseWrapper.java
+++ src/main/java/com/iucyh/flip/core/response/SuccessResponseWrapper.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
  * 컨트롤러가 반환한 도메인 응답 DTO를 {@link SuccessResponse}로 자동 래핑
- * <p><b>제외</b>: 이미 {@link SuccessResponse}로 래핑된 응답, {@link FailResponse} (전역 예외 핸들러를 통해 래핑됨), 기타 JSON 본문이 아닌 것들 ({@code CharSequence}, {@code byte[]}, {@code Resource})</p>
+ * <p><b>제외</b>: 이미 {@link SuccessResponse}로 래핑된 응답, {@link FailResponse} (전역 예외 핸들러를 통해 생성됨), 기타 JSON 본문이 아닌 것들 ({@code CharSequence}, {@code byte[]}, {@code Resource})</p>
  */
 @RestControllerAdvice(basePackages = "com.iucyh.flip") // 직접 개발한 API에 대해서만 적용 (프레임워크나 라이브러리가 제공하는 API는 제외)
 public class SuccessResponseWrapper implements ResponseBodyAdvice<Object> {
